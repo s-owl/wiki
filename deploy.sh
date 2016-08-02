7c0c1fa4a2e12acd620820890de49c396fb32986
sukso96100 <sukso96100@gmail.com>
2016-08-02 12:54:20 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,52 +1,3 @@
-#!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
-
-SOURCE_BRANCH="src"
-TARGET_BRANCH="master"
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    npm install -g gitbook-cli
-    gitbook build
-    exit 0
-fi
-
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO _book && ls
-cd _book && ls
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd .. && ls
-
-# Clean out existing contents
-rm -rf _book/**/* || exit 0
-
-# Run our compile script
-npm install -g gitbook-cli
-gitbook build && ls
-
-# Now let's go have some fun with the cloned repo
-cd _book && ls
-git config user.name "Travis CI"
-git config user.email "we.sss.you@gmail.com"
-
-# If there are no changes (e.g. this is a README update) then just bail.
-# if [ -z `git diff --exit-code` ]; then
-#     echo "No changes to the spec on this push; exiting."
-#     exit 0
-# fi
-
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
-
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
@@ -57,5 +8,17 @@ chmod 600 deploy_rsa
 eval `ssh-agent -s`
 ssh-add deploy_rsa
 
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git config --global user.name "WikiUpdater"
+git config --global user.email "we.sss.you@gmail.com"
+
+git clone git@github.com:skhu-sss/skhu-sss.github.io.git deploy && ls
+cd deploy && ls
+git checkout master && ls
+rm -rf * && ls
+cd .. & ls
+npm install -g gitbook-cli
+ls && gitbook build && ls
+cp -a _book/. deploy/
+cd deploy && ls
+git add . && git commit -m "Updated Contents (via Travis CI)"
+git push origin master
