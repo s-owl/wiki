40c7331ef4a31f8c6fae30e4182af5a882e37212
sukso96100 <sukso96100@gmail.com>
2017-02-28 19:09:16 +0900

    Updated Deploy Settings

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,19 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 SHA=`git rev-parse --verify HEAD`
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_rsa.enc -out deploy_rsa -d
-chmod 600 deploy_rsa
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in wiki_deploy_key.enc -out wiki_deploy_key -d
+chmod 600 wiki_deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_rsa
+ssh-add wiki_deploy_key
 
 git config --global user.name "WikiUpdater"
 git config --global user.email "we.sss.you@gmail.com"
 
 export COMMITID = git rev-parse --short HEAD
 
-git clone git@github.com:skhu-sss/skhu-sss.github.io.git deploy && ls
+git clone git@github.com:skhu-sss/wiki.git deploy && ls
 cd deploy && ls
-git checkout master
+git checkout gh-pages
 ls
 rm -rf *
 ls
@@ -34,10 +34,10 @@ rm .travis.yml
 ls
 rm deploy.sh
 ls
-rm deploy_rsa.enc
+rm wiki_deploy_key.enc
 ls
-rm deploy_rsa
+rm wiki_deploy_key
 ls
 git add --all
 git commit -m "Updated Contents (via Travis CI) [${SHA}]"
-git push origin master
+git push origin gh-pages
